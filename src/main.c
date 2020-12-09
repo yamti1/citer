c8a075c2f64fafadff6ca5e2b1986d6545e372b3
yamti <yam.tirosh@gmail.com>
2020-12-09 22:47:49 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,20 +3,49 @@
 #include "citer.h"
 
 
-int is_even(void* value_ptr) {
-    int value = *((int*)value_ptr);
-    return value % 2 == 0;
+void basic_iteration() {
+    printf("Basic iteration:\n");
+    int arr[] = {1, 2, 5, 9};
+    BaseIterator iterator = iter(arr, sizeof(*arr), 4);
+
+    int element = 0;
+    while (next(&iterator, BASE_ITERATOR, &element)) {
+        printf("%i\n", element);
+    }
 }
 
-int main() {
-    int my_array[] = {1, 2, 3, 5, 6, 10, 11, 29};
-    ReversedIterator iterator1 = reversed(my_array, sizeof(int), 8);
-    FilterIterator iterator2 = filter(&is_even, REVERSED_ITERATOR, &iterator1);
+void reversed_iteration() {
+    printf("Reversed iteration:\n");
+    int arr[] = {1, 2, 5, 9};
+    ReversedIterator iterator = reversed(arr, sizeof(*arr), 4);
+
+    int element = 0;
+    while (next(&iterator, REVERSED_ITERATOR, &element)) {
+        printf("%i\n", element);
+    }
+}
+
+int is_odd(void* value_ptr) {
+    int value = *((int*)value_ptr);
+    return value % 2 == 1;
+}
+
+void filter_reversed_iteration() {
+    printf("Filter reversed iteration:\n");
+    int arr[] = {1, 2, 5, 9};
+    ReversedIterator iterator1 = reversed(arr, sizeof(*arr), 4);
+    FilterIterator iterator2 = filter(&is_odd, REVERSED_ITERATOR, &iterator1);
 
     int element = 0;
     while (next(&iterator2, FILTER_ITERATOR, &element)) {
         printf("%i\n", element);
     }
+}
+
+int main() {
+    basic_iteration();
+    reversed_iteration();
+    filter_reversed_iteration();
     
     return 0;
 }
